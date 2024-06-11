ee81edaca0cb8b33c49e3f72f959da618c29cc15
ERROR938 <sebastien.orgue@gmail.com>
2024-06-11 15:04:22 +0200

    Fix : update sql

--- radars.sql
+++ radars.sql
@@ -1,6 +1,8 @@
 CREATE TABLE `radars` (
     `id` INT NOT NULL AUTO_INCREMENT, 
     `position` LONGTEXT NOT NULL, 
+    `heading` longtext NOT NULL,
+    `view` int(2) NOT NULL,
     `mph` INT(3) NOT NULL, 
     `name` VARCHAR(200) NOT NULL, 
     PRIMARY KEY (`id`)
